837b97ee9a269bc2e63da9ad533fe2c621ad3fa0
Eva <eva.sanchezguerrero@futureconnections.com>
2023-03-02 10:22:39 +0100

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+        setInputValue( event.target.value )
+    }
+
+    const onSubmit = (event) => {
+          event.preventDefault();
+        
+          if (inputValue.trim().length <= 1) return;
+
+          onNewCategory( inputValue.trim() );
+          setInputValue('');
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+            <input
+                type='text'
+                placeholder='Search gif'
+                value={ inputValue }
+                onChange={ onInputChange }
+            />
+        </form>
+    )
+}
